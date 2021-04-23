32261cc1d7de9455fe099d5836427bb124c396a6
Sander Saares <sasaares@microsoft.com>
2021-04-23 12:32:28 +0300

    Remove unneccessary variable

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -4,4 +4,4 @@ Import-Module sandersaares.devops-scripts
 
 $path = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_ASSEMBLYINFOPATH
 
-$version = Set-DotNetBuildAndVersionStrings -assemblyInfoPath $path -commitId $ENV:GITHUB_SHA -primaryBranchName $env:INPUT_PRIMARYBRANCH
+Set-DotNetBuildAndVersionStrings -assemblyInfoPath $path -commitId $ENV:GITHUB_SHA -primaryBranchName $env:INPUT_PRIMARYBRANCH
